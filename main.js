8be68cf7932d3fa6887a851866ee0b3426f2f22a
shadeed-winner <87156808+shadeed-winner@users.noreply.github.com>
2022-02-09 20:55:57 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/AJ3d43GC1/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak() {
+    var synth =  window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "The second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+if (error) {
+  console.error(error);
+} else {
+  console.log(results);
+  document.getElementById("result_gesture_name").innerHTML = results[0].label;
+  document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+  prediction_1 = results[0].label;
+  prediction_2 = results[1].label;
+  speak();
+  if(results[0].label == "Open")
+  {
+      document.getElementById("update_gesture").innerHTML = "(1st image)";
+  }
+  if(results[0].label == "Weird")
+  {
+      document.getElementById("update_gesture").innerHTML = "(4nd image)";
+  }
+  if(results[0].label == "???")
+  {
+      document.getElementById("update_gesture").innerHTML = "(3rd image)";
+  }
+  if(results[0].label == "Spider-man")
+  {
+      document.getElementById("update_gesture").innerHTML = "(2nd image)";
+  }
+  if(results[0].label == "Finger guns")
+  {
+      document.getElementById("update_gesture").innerHTML = "(5th image)";
+  }
+
+  if(results[1].label == "Open")
+  {
+      document.getElementById("update_gesture2").innerHTML = "(1st image)";
+  }
+  if(results[1].label == "Weird")
+  {
+      document.getElementById("update_gesture2").innerHTML = "(4th image)";
+  }
+  if(results[1].label == "???")
+  {
+      document.getElementById("update_gesture2").innerHTML = "(3rd image)";
+  }
+  if(results[1].label == "Spider-man")
+  {
+      document.getElementById("update_gesture2").innerHTML = "(2nd image)";
+  }
+  if(results[1].label == "Finger guns")
+  {
+      document.getElementById("update_gesture2").innerHTML = "(5th image)";
+  }
+}
+}
